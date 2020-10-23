ccef58bb1e1e0f5cfa183ceb3d1e1e538a0ebe3c
zhanggaojie <zhanggaojie@yy.com>
2020-10-23 18:37:10 +0800

    fix: orderByClause --> example.orderByClause

--- src/main/java/org/mybatis/generator/codegen/mybatis3/xmlmapper/elements/SelectByExampleWithoutBLOBsElementGenerator.java
+++ src/main/java/org/mybatis/generator/codegen/mybatis3/xmlmapper/elements/SelectByExampleWithoutBLOBsElementGenerator.java
@@ -68,7 +68,7 @@ public class SelectByExampleWithoutBLOBsElementGenerator extends
 
         ifElement = new XmlElement("if");
         ifElement.addAttribute(new Attribute("test", "example != null and example.orderByClause != null"));
-        ifElement.addElement(new TextElement("order by ${orderByClause}"));
+        ifElement.addElement(new TextElement("order by ${example.orderByClause}"));
         answer.addElement(ifElement);
 
         ifElement = new XmlElement("if");
